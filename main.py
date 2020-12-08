10d16972122ac094e38e70c13ae1f2808a918047
Naveen <>
2020-12-08 02:11:02 +0000

    Added threading

--- main.py
+++ main.py
@@ -8,11 +8,46 @@ import xir.graph
 import xir.subgraph
 import os
 import sys
-import math
+import threading
+import queue
 
 colorB = (128, 232, 70, 156, 153, 153, 30,  0,   35, 152, 180, 60,  0,  142, 70,  100, 100, 230, 32)
 colorG = (64,  35, 70, 102, 153, 153, 170, 220, 142, 251, 130, 20, 0,  0,   0,   60,  80,  0,   11)
 colorR = (128, 244, 70,  102, 190, 153, 250, 220, 107, 152, 70,  220, 255, 0,   0,   0,   0, 0, 119)
+
+global isCapturing
+
+def streamCapture(url, queueIn):
+    global isCapturing
+    print('Capture stream from {}'.format(url))
+    video = pafy.new(url)
+    stream = video.streams[2]
+    cap = cv2.VideoCapture(stream.url)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        isCapturing = True
+
+        if not ret:
+            isCapturing = False
+            break
+        queueIn.put(frame)
+
+    cap.release()
+
+
+def display(ip, port, queueOut):
+    global isCapturing
+    width, height = 512, 256
+    pipeline = 'appsrc ! videoconvert ! video/x-raw,format=I420,width={},height={} ! videoconvert ! jpegenc ! rtpjpegpay ! queue ! udpsink host={}  port={}'.format(width, height, ip, port)
+    out = cv2.VideoWriter(pipeline, cv2.CAP_GSTREAMER, 0, 30, (width, height), True)
+    print(pipeline)
+
+    while isCapturing:
+        frame = queueOut.get()
+        #print('Write frame')
+        out.write(frame)
+
+    out.release()
 
 def label_to_pixel(img):
     result = np.zeros((img.shape[0], img.shape[1], 3), dtype='uint8')
@@ -21,7 +56,9 @@ def label_to_pixel(img):
 
     return result
 
-def runSegmentation(dpu, frame, out):
+def runSegmentation(worker, dpu, queueIn, queueout):
+    global isCapturing
+    print('Worker: {}'.format(worker))
     inputTensors  = dpu.get_input_tensors()
     outputTensors = dpu.get_output_tensors()
     inputHeight   = inputTensors[0].dims[1]
@@ -34,28 +71,33 @@ def runSegmentation(dpu, frame, out):
     shapeIn       = (batchSize,) + tuple([inputTensors[0].dims[i] for i in range(inputTensors[0].ndim)][1:])
 
     #print(outputWidth, outputHeight)
-    frame = cv2.resize(frame, (inputWidth, inputHeight), interpolation=cv2.INTER_LINEAR) 
-    img = frame.astype(np.float32)
-    mean = (104, 117, 123);
-    img = img - mean
+    while isCapturing:
+        if queueIn.empty():
+            continue
+        frame = queueIn.get() 
+        frame = cv2.resize(frame, (inputWidth, inputHeight), interpolation=cv2.INTER_LINEAR) 
+        img = frame.astype(np.float32)
+        mean = (104.0, 117.0, 123.0);
+        img = img - mean
 
-    outputData = []
-    inputData  = []
-    outputData.append(np.empty((batchSize, outputHeight, outputWidth, outputChannel), dtype = np.float32, order = 'C'))
-    inputData.append(np.empty((shapeIn), dtype = np.float32, order = 'C'))
+        outputData = []
+        inputData  = []
+        outputData.append(np.empty((batchSize, outputHeight, outputWidth, outputChannel), dtype = np.float32, order = 'C'))
+        inputData.append(np.empty((shapeIn), dtype = np.float32, order = 'C'))
 
-    imageRun = inputData[0]
-    imageRun[0, ...] = img.reshape(inputTensors[0].dims[1],inputTensors[0].dims[2],inputTensors[0].dims[3])
+        imageRun = inputData[0]
+        imageRun[0, ...] = img.reshape(inputTensors[0].dims[1],inputTensors[0].dims[2],inputTensors[0].dims[3])
 
-    job_id = dpu.execute_async(inputData, outputData)
-    dpu.wait(job_id)
+        job_id = dpu.execute_async(inputData, outputData)
+        dpu.wait(job_id)
 
-    pred_label = np.argmax(outputData[0][0], axis=-1) 
-    seg_img = label_to_pixel(pred_label)
-    seg_img = cv2.resize(seg_img, (frame.shape[1], frame.shape[0]), interpolation=cv2.INTER_NEAREST) 
-    out_img = cv2.addWeighted(frame, 0.4, seg_img, 0.6, 0)
+        pred_label = np.argmax(outputData[0][0], axis=-1) 
+        seg_img = label_to_pixel(pred_label)
+        seg_img = cv2.resize(seg_img, (frame.shape[1], frame.shape[0]), interpolation=cv2.INTER_NEAREST) 
+        out_img = cv2.addWeighted(frame, 0.4, seg_img, 0.6, 0)
 
-    out.write(out_img)
+        #print('Prediction done')
+        queueout.put(out_img)
 
 
 def get_subgraph(g):
@@ -73,24 +115,42 @@ if __name__ == "__main__":
     g = xir.graph.Graph.deserialize(pathlib.Path(model))
     subgraphs = get_subgraph (g)
     assert len(subgraphs) == 1 # only one DPU kernel
-    dpu_runner =  runner.Runner(subgraphs[0], "run")
-    url   = 'https://www.youtube.com/watch?v=lkIJYc4UH60'
-    video = pafy.new(url)
-    stream = video.streams[2]
-    cap = cv2.VideoCapture(stream.url)
-    #pipeline  = 'appsrc ! videoconvert !  video/x-raw,format=I420,width=640,height=360 ! videoconvert ! jpegenc ! rtpjpegpay ! queue ! udpsink host=192.168.3.2  port=1234'
-    pipeline  = 'appsrc ! videoconvert !  video/x-raw,format=I420,width=512,height=256 ! videoconvert ! jpegenc ! rtpjpegpay ! queue ! udpsink host=192.168.3.2  port=1234'
-    #out = cv2.VideoWriter(pipeline, cv2.CAP_GSTREAMER, 0, 30, (640,360), True)
-    out = cv2.VideoWriter(pipeline, cv2.CAP_GSTREAMER, 0, 30, (512, 256), True)
-    
-    print('Reading frames...')
-    while cap.isOpened():
-        ret, frame = cap.read()
 
-        if not ret:
-            break
+    global isCapturing
+    ip      = '192.168.3.2'
+    port    = '1234'
+    url     = 'https://www.youtube.com/watch?v=lkIJYc4UH60'
+    threads = 2
 
-        runSegmentation(dpu_runner, frame, out)
-    
-    cap.release()
-    out.release()
+    all_dpu_runners = [];
+    for i in range(int(threads)):
+        all_dpu_runners.append(runner.Runner(subgraphs[0], "run"));
+
+    # Init synchronous queues for inter-thread communication
+    queueIn = queue.Queue()
+    queueOut = queue.Queue()
+
+    # Launch threads
+    threadAll = []
+    sc = threading.Thread(target=streamCapture, args=(url, queueIn))
+    threadAll.append(sc)
+
+    for i in range(threads):
+        tw = threading.Thread(target=runSegmentation, args=(i, all_dpu_runners[i], queueIn, queueOut))
+        threadAll.append(tw)
+
+    td = threading.Thread(target=display, args=(ip, port, queueOut))
+    threadAll.append(td)
+
+    isCapturing = True
+    for x in threadAll:
+        x.start()
+
+    # Wait for all threads to stop
+    for x in threadAll:
+        x.join()
+
+    # clean up resources
+    for runner in all_dpu_runners:
+        del runner
+
